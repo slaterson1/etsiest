1e1c84b1a120205af09fbed225871b3e26b18223
slatersan1 <cdlee212@gmail.com>
2016-03-17 15:17:32 -0400

    basic project

---
+++ Gemfile
@@ -0,0 +1,12 @@
+source 'https://rubygems.org'
+
+gem "httparty"
+gem "etsy"
+
+group :development
+	
+gem "pry"
+
+end
+# Specify your gem's dependencies in etsiest.gemspec
+gemspec
---
+++ test/etsiest_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+
+class EtsiestTest < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::Etsiest::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
